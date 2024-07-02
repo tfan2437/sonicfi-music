522d3aa9dc5dbf8cd27a8a5a3ba6fae5973005b1
tfan2437 <tfan2437@gmail.com>
2024-07-01 23:39:26 -0400

    initial commit for streamfi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Player from "./components/Player";
+import Sidebar from "./components/Sidebar";
+import Display from "./components/Display";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+    </div>
+  );
+};
+
+export default App;
